4f9529f61c023f2c9b336fd4153742aa9f7fa229
troykollar <troykollar@gmail.com>
2019-09-17 20:34:11 -0400

    updates to existing code

--- Lab3a/Test/tb.v
+++ Lab3a/Test/tb.v
@@ -4,7 +4,8 @@ module tb ();
 
 reg clk;
 reg button;
-wire button_out;
+wire [7:0] button_out;
+reg reset;
 
 
 initial clk = 0;
@@ -13,11 +14,11 @@ initial forever #5 clk = ~clk;
 initial
   begin
 #0	button = 0;
-#700	button = 1;
-#1000  	button = 0;
-#1500 	button = 1;
-#2000	button = 0;
-#6000    $display("made it to 6000 @ %t", $time);
+#0 	reset = 0;
+#100	button = 1;
+#500	 button = 0;
+#800 	button = 1;
+#1000	button = 0;
 
 #6000   $finish;
   end
@@ -26,7 +27,8 @@ initial
 debounce_count button1 (
                  .button(button),
                  .clk(clk),
-		 .button_count(button_out)
+		 .answer(button_out),
+		 .reset(reset)
  );
 
 
