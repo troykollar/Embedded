4712e09faff99c78a29eecbb80a977ca407e1961
troykollar <troykollar@gmail.com>
2019-09-18 22:00:48 -0400

    Created state machine binary file

--- Lab5/State.v
+++ Lab5/State.v
@@ -2,9 +2,33 @@
 
 module state_machine(input wire clk, input wire sw1, input wire sw2, input wire button, input wire reset, output reg detected_signal, output reg [7:0] outleds = 8'b0000_0000);
 
-reg [3:0] state = 4'b0000;
+//Debounce Button Code//
+reg 			button_reg;
+reg 			button_sync;
+reg [31:0]	button_count;
+reg [11:0]	answer;
+parameter DEBOUNCE_DELAY = 32'd0_500_000;
 
-always @(posedge button)
+always @(posedge clk) button_reg <= button;
+always @(posedge clk) button_sync <= !button_reg;
+
+assign		button_done = (button_count == DEBOUNCE_DELAY);
+assign		button_click = (button_count == DEBOUNCE_DELAY - 1);
+
+always @(posedge clk)
+		if(!button_sync) 		button_count <= 0;
+		else if(button_done) 	button_count <= button_count;
+		else 							button_count <= button_count + 1;
+		
+always @(posedge clk)
+		if (button_click)	answer <= answer + 1;
+		else if (!reset)	answer <= 0;
+		else					answer <= answer;
+
+
+//Actual State Machine Code//
+reg [3:0] state = 4'b0000;
+always @(answer)
 	case(state)
 		0:
 			if (reset)		state <= 0;
