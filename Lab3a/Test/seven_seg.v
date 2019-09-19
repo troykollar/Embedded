0da7117a24409ea0bb090a9fd7d289a365814916
troykollar <troykollar@gmail.com>
2019-09-18 21:05:18 -0400

    Generated new bin file for Lab3

--- Lab3a/Test/seven_seg.v
+++ Lab3a/Test/seven_seg.v
@@ -1,47 +1,36 @@
-module counter(clk,Switch4,Switch5, reset, anodes, cathodes, outleds);
-input Switch4;
-input Switch5;
-input clk;
-input reset;
+module counter(input clk, input Switch4, input Switch5, input reset, output reg [2:0] anodes, output reg [7:0] cathodes, output reg [7:0] outleds);
 
-output [2:0]anodes;
-output [7:0]cathodes;
-output [7:0]	outleds;
-
-reg [2:0]anodes;
-reg [7:0]cathodes;
-reg [7:0]outleds;
-reg button_reg;
-reg button_reg2;
-reg button_sync;
-reg button_sync2;
-reg [31:0]	button_count;
-reg [31:0]  button_count2;
+reg button1_reg;
+reg button2_reg;
+reg button1_sync;
+reg button2_sync;
+reg [31:0]	button1_count;
+reg [31:0]  button2_count;
 reg   [11:0]   answer;   
-parameter DEBOUNCE_DELAY = 32'd0_500_000;   /// 10nS * 1M = 10mS
+parameter DEBOUNCE_DELAY1 = 32'd0_500_000;   /// 10nS * 1M = 10mS
 parameter DEBOUNCE_DELAY2 = 32'd0_500_000;   /// 10nS * 1M = 10mS
 
-always @(posedge clk)   button_reg   <= Switch4;
-always @(posedge clk)   button_sync  <= !button_reg;
+always @(posedge clk)   button1_reg   <= Switch4;
+always @(posedge clk)   button1_sync  <= !button1_reg;
 
-always @(posedge clk)   button_reg2   <= Switch5;
-always @(posedge clk)   button_sync2  <= !button_reg2;
+always @(posedge clk)   button2_reg   <= Switch5;
+always @(posedge clk)   button2_sync  <= !button2_reg;
 
-assign                  button_done  = (button_count == DEBOUNCE_DELAY); 
-assign                  button_click = (button_count == DEBOUNCE_DELAY - 1); 
+assign                  button_done  = (button1_count == DEBOUNCE_DELAY1); 
+assign                  button_click = (button1_count == DEBOUNCE_DELAY1 - 1); 
 
-assign                  button_done2  = (button_count2 == DEBOUNCE_DELAY2); 
-assign                  button_click2 = (button_count2 == DEBOUNCE_DELAY2 - 1); 
-
-always@(posedge clk)
-		if(!button_sync)  button_count <= 0;
-		else if (button_done) button_count <= button_count;
-		else 					button_count <= button_count + 1;
+assign                  button_done2  = (button2_count == DEBOUNCE_DELAY2); 
+assign                  button_click2 = (button2_count == DEBOUNCE_DELAY2 - 1); 
 
 always@(posedge clk)
-		if(!button_sync2)  button_count2 <= 0;
-		else if (button_done2) button_count2 <= button_count2;
-		else 					button_count2 <= button_count2 + 1;
+		if(!button1_sync)  button1_count <= 0;
+		else if (button_done) button1_count <= button1_count;
+		else 					button1_count <= button1_count + 1;
+
+always@(posedge clk)
+		if(!button2_sync)  button2_count <= 0;
+		else if (button_done2) button2_count <= button2_count;
+		else 					button2_count <= button2_count + 1;
 			
 always @(posedge clk)
 		if(button_click) answer <= answer + 1;
@@ -65,20 +54,20 @@ always @(posedge clk)
 	     else if(anode_clk)   anodes <= {anodes[0],anodes[2:1]}; // rotate
 	     else                 anodes <=  anodes;  
 
-reg [3:0] cathode_select;
+reg [3:0] cathod_S;
 
-always @(cathode_select or answer)
+always @(cathod_S or answer)
        case({anodes})
-	      3'b011:  cathode_select = answer[11:8]; 
-	      3'b101:  cathode_select = answer[7:4]; 
-	      3'b110:  cathode_select = answer[3:0]; 
-	      default:  cathode_select = 4'h0; 
+	      3'b011:  cathod_S = answer[11:8]; 
+	      3'b101:  cathod_S = answer[7:4]; 
+	      3'b110:  cathod_S = answer[3:0]; 
+	      default:  cathod_S = 4'h0; 
       endcase
 
 //wire dp = 1; //!(anodes == 4'b1011); 
 
-always @(cathode_select)
-		case(cathode_select)
+always @(cathod_S)
+		case(cathod_S)
 	       4'h0:  cathodes = {8'b11000000};
 			 4'h1:  cathodes = {8'b11111001};
 			 4'h2:  cathodes = {8'b10100100};
